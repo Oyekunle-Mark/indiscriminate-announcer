6d0a9b9b1d989fcd53e894bdaadb95829ba0307f
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-04 13:28:03 +0100

    Refactored the index.js file

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function initMap() {
   const input = document.getElementById('mapSuggestions');
-  const autoComplete = new google.maps.places.Autocomplete(input);
+  const autocomplete = new google.maps.places.Autocomplete(input);
 
-  // autoComplete.addEventListener('place_changed', () => {
-  //   var place = autocomplete.getPlace();
-  //   document.getElementById('location-snap').innerHTML =
-  //     place.formatted_address;
-  //   document.getElementById(
-  //     'lat-span',
-  //   ).innerHTML = place.geometry.location.lat();
-  //   document.getElementById(
-  //     'lon-span',
-  //   ).innerHTML = place.geometry.location.lng();
-  // });
-}
+  autocomplete.addListener('place_changed', () => {
+    const place = autocomplete.getPlace();
+    document.getElementById('location-snap').innerHTML =
+      place.formatted_address;
+    document.getElementById(
+      'lat-span',
+    ).innerHTML = place.geometry.location.lat();
+    document.getElementById(
+      'lon-span',
+    ).innerHTML = place.geometry.location.lng();
+  });
+};
